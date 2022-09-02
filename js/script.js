984d43ac87b80088191ec56b69e54f095ed7f276
Mateuscruz19 <fdsmeuirmao2@outlook.com>
2022-09-02 10:13:42 -0300

    versão mobile desktop 1 pronto

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function receberQuizes(resposta){
      let gradeQuiz=document.querySelector('.gridquizzes')
      let renderizando='' //renderizar os quizzes
     for(i=0;i<containerDeQuizes.length;i++){
-            renderizando+= `<li class="quizesgeral">
+            renderizando+= `<li class="quizesgeral" onclick="exibirquiz()">
             <img src="${containerDeQuizes[i].image}" data-${containerDeQuizes[i].id}>
             <p>${containerDeQuizes[i].title}</p>
         </li>
@@ -22,7 +22,23 @@ function receberQuizes(resposta){
     gradeQuiz.innerHTML=renderizando
 }
 
+
+
 function erro(_errou) {
-    console.log(Ops, falhou, em, carregar, os, quizzes);
+    Alert("Ops, falhou em carregar os quizzes,iremos recarregar a sua pagina em 4 segundos.");
+    setInterval(window.location.reload() ,4000);
 }
+
+function exibirquiz(){
+    console.log('clicou');
+    let quizusuario =document.querySelector('.quizes-usuario');
+    let quiztodos =document.querySelector('.conteiner-de-quizes');
+    let abaquiz = document.querySelector('.div-perguntas');
+    let banner = document.querySelector('.banner-superior');   
+    quizusuario.style.display='none';
+    quiztodos.style.display='none';
+    abaquiz.classList.remove("escondido");
+    banner.classList.remove("escondido");
+}
+
 
