2e1bc46626606d6d8560be4ba0f2ff1e340508df
Isaias <figueiredo.isaias@gmail.com>
2022-09-05 14:27:14 -0300

    resolvido  problema pg2

--- js/isaias.js
+++ js/isaias.js
@@ -3,6 +3,8 @@ function respostaSelecionada(parametro){
     console.log(apiSelecionada)
     console.log(apiSelecionada)
     console.log(parametro)
+    let imge=document.querySelector('.imagem')
+    imge.classList.add('borda')
     if(parametro.dataset.check==='true'){
         alert('acertou')
     }
